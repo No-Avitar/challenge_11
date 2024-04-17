5b34750af645e53fc44ba3a168c250f26683afc2
Davis Balan <balandavis6@gmail.com>
2024-04-16 21:33:13 -0500

    more stuff added

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,9 +1,12 @@
 const notes = require('express').Router();
 
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
+
 const notesData = require('../db/db.json')
 
 notes.get('/', (req, res) => {
-    res.json(notesData)
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 notes.post('/', (req, res) => {
@@ -12,6 +15,20 @@ notes.post('/', (req, res) => {
     newNote = req.body
     newNote.id = Math.floor((1 + Math.random())*0x10000).toString(16).substring(1)
     console.log(newNote)
+    readAndAppend(newNote, "./db/db.json")
+
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+})
+
+notes.delete('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => {
+        const oldData = data.find
+    })
 })
 
 module.exports = notes;
