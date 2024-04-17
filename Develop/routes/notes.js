220efa26b8bcd7f2aa391773a6a75191e0e88a36
Davis Balan <balandavis6@gmail.com>
2024-04-16 21:05:10 -0500

    what i have so far

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -10,7 +10,8 @@ notes.post('/', (req, res) => {
     console.info("request to save note")
     console.log(req.body)
     newNote = req.body
-    newNote.id = Math.floor((1 + Math.random()) * 100000).toString
+    newNote.id = Math.floor((1 + Math.random())*0x10000).toString(16).substring(1)
+    console.log(newNote)
 })
 
 module.exports = notes;
