5f166c980e999bd5192d2e62cd527d7f4e868755
Davis Balan <balandavis6@gmail.com>
2024-05-06 18:54:29 -0500

    fixed file structure and added a start script

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use('/api', api)
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '../index.html'))
+  res.sendFile(path.join(__dirname, './index.html'))
 );
 
 app.get('/notes', (req, res) =>
